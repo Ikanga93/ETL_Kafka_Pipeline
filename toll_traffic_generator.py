0892db5632d3e2804aaa5f3a3c4e2a385f8a39c9
gekuke1 <ikanga93@gmail.com>
2024-06-14 14:32:01 -0400

    one more commit

--- toll_traffic_generator.py
+++ toll_traffic_generator.py
@@ -1,16 +1,25 @@
 """
 Top Traffic Simulator
 """
+
+# Import necessary modules from Python standard library
 from time import sleep, time, ctime
 from random import random, randint, choice
 from kafka import KafkaProducer
+
+# Initialize a kafka producer to send messages to kafka.
+# The producer will connect to the kafka server running on localhost at port 9092.
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
+# Define the Kafka topic to which messages will be sent.
 TOPIC = 'toll'
 
+# Define a tuple of vehicle types, where the probability of a selecting a 'car' is higher than the other vehicle types.
 VEHICLE_TYPES = ("car", "car", "car", "car", "car", "car", "car", "car",
                  "car", "car", "car", "truck", "truck", "truck",
                  "truck", "van", "van")
+
+# Start a loop that will run 100,000 times to simulate vehicle data
 for _ in range(100000):
     vehicle_id = randint(10000, 10000000)
     vehicle_type = choice(VEHICLE_TYPES)
